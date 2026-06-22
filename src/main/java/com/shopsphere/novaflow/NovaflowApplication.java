23efc1d4e89713fa5d9bc5d6058bb4070e6402cf
Anu Maurya <santosh0maurya99@gmail.com>
2026-06-22 23:27:00 +0530

    "Add NovaFlow SaaS landing page with pricing, FAQ and contact form"

---
+++ src/main/java/com/shopsphere/novaflow/NovaflowApplication.java
@@ -0,0 +1,13 @@
+package com.shopsphere.novaflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovaflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopsphere/novaflow/NovaflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopsphere.novaflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
